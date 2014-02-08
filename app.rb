c3fc91a044a7167b4f1f5a55b2fb0ced5bd7211a
Jon Yom-Tov <jon.yomtov@gmail.com>
2014-02-09 00:15:51 +0200

    Added like button and GA, added some CSS for results div, deleted some unnecessary lines (not code)

--- app.rb
+++ app.rb
@@ -2,51 +2,42 @@ require 'sinatra'
 require 'sequel'
 require 'json'
 
-#class App < Sinatra::Base
+configure do
+  db_url = (production? ? ENV['DATABASE_URL'] : 'postgres://yoni:telaviv@localhost/knesset_donations')	
+  Sequel::Model.db = Sequel.connect db_url
+  require './models/donor'
+  require './models/donation'
+end
 
-	
-	
+helpers do
+	def get_donations(donor_id)
+		dataset = Donation.
+			graph(:donors, :id => :donor_id).
+			graph(:candidates, :id => :donations__candidate_id).
+			graph(:parties, :id => :party_id).
+			where(:donor_id => donor_id)
+		results = dataset.inject([]) do |mem, item|
+			currency = (item[:currency] == 'usd' ? '$' : '&#8362;')
+			mem << [item[:name], item[:parties_name], item[:candidates_name], currency + item[:amount].to_s, item[:date]]
+		end	
 
-	configure do
-	  db_url = (production? ? ENV['DATABASE_URL'] : 'postgres://yoni:telaviv@localhost/knesset_donations')	
-	  puts "*************** #{db_url}"	
-	  Sequel::Model.db = Sequel.connect db_url
-	  require './models/donor'
-	  require './models/donation'
+		results
 	end
+end
 
-	helpers do
-		def get_donations(donor_id)
-			dataset = Donation.
-				graph(:donors, :id => :donor_id).
-				graph(:candidates, :id => :donations__candidate_id).
-				graph(:parties, :id => :party_id).
-				where(:donor_id => donor_id)
-			results = dataset.inject([]) do |mem, item|
-				currency = (item[:currency] == 'usd' ? '$' : '&#8362;')
-				mem << [item[:name], item[:parties_name], item[:candidates_name], currency + item[:amount].to_s, item[:date]]
-			end	
+get '/' do
+  begin
+  	  @donors = Donor.all.
+	  	inject([]){|mem, donor| mem << {:value => donor.id, :label => donor.name}}.
+	  	to_json
+	  erb :index		
+    rescue Exception => e
+	  halt 500
+    end		  
+end
 
-			results
-		end
-	end
+get '/donor/:id' do
+	@donations = get_donations(params[:id])
 
-	get '/' do
-	  # puts "*** index"	
-	  # "hi"
-	  begin
-	  	@donors = Donor.all.
-		  	inject([]){|mem, donor| mem << {:value => donor.id, :label => donor.name}}.
-		  	to_json
-		erb :index		
-  	  rescue Exception => e
-  		halt 500
-  	  end		  
-	end
-
-	get '/donor/:id' do
-		@donations = get_donations(params[:id])
-
-		erb :donations
-	end
-#end
+	erb :donations
+end
